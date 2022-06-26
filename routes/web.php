0f4f8d4b2e40065ddec29a616166c9355f0f1582
funkylen <funkylen@gmail.com>
2022-06-27 02:48:38 +0300

    Route permissions

--- routes/web.php
+++ routes/web.php
@@ -4,13 +4,9 @@ use App\Http\Controllers\BookController;
 use App\Http\Controllers\ReadLogController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
-Route::resource('books', BookController::class);
-Route::resource('books.read_logs', ReadLogController::class)->except('index');
+Route::middleware('auth')->group(function () {
+    Route::redirect('/', 'books');
+    Route::resource('books', BookController::class);
+    Route::resource('books.read_logs', ReadLogController::class)->except('index');
+});
