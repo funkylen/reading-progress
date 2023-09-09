42ebfd7e9c01d9f27cca6ae1034faa2187df78a5
funkylen <funkylen@gmail.com>
2023-09-10 00:05:38 +0300

    drop feedback

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,6 @@
 
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\BookController;
-use App\Http\Controllers\FeedbackController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ReadLogController;
 use Illuminate\Support\Facades\Route;
@@ -13,8 +12,6 @@ Route::get('/', [HomeController::class, 'index']);
 
 Route::get('/auth/google/redirect', [LoginController::class, 'googleRedirect'])->name('auth.google.redirect');
 Route::get('/auth/google/callback', [LoginController::class, 'googleCallback'])->name('auth.google.callback');
-
-Route::resource('feedback', FeedbackController::class)->only('create', 'store');
 
 Route::middleware('auth')->group(function () {
     Route::resource('books', BookController::class);
